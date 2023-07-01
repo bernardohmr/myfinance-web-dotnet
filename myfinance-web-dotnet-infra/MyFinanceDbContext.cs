b213203082c9b03a983de24f2a997beba8437355
Bernardo Rodrigues <bernardohmr@gmail.com>
2023-07-01 15:10:26 -0300

    refactor: env var for database

--- myfinance-web-dotnet-infra/MyFinanceDbContext.cs
+++ myfinance-web-dotnet-infra/MyFinanceDbContext.cs
@@ -1,16 +1,23 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using myfinance_web_dotnet_domain.Entities;
 
 namespace myfinance_web_dotnet_infra;
 
 public class MyFinanceDbContext : DbContext
 {
+    private readonly IConfiguration _configuration;
     public DbSet<PlanoConta> PlanoConta {get; set; }
     public DbSet<Transacao> Transacao {get; set; }
 
+    public MyFinanceDbContext(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        optionsBuilder.UseSqlServer(@"Server=localhost,1433;Database=myfinance;User=sa;Password=Odr@nreb123;Encrypt=False;TrustServerCertificate=False");
+        var connectionString = _configuration.GetConnectionString("Database");
+        optionsBuilder.UseSqlServer(connectionString);
     }
 
 }
